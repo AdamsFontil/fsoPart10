dfdc3240ac9250d921fac0d9c319a961280bcd0e
AdamsFontil <adamsfuntil@gmail.com>
2023-06-26 21:57:25 -0400

    finish 10.1

---
+++ rate-repository-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is for the actual exercise testing</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
